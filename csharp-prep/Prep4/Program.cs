68d8354550a0df5d1857ddf7e2b828cfdceb4c5d
trevheg <trevorhegstrom@gmail.com>
2024-01-11 11:23:58 -0800

    Week 1 preps 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,80 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        // Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> userNumbers = new List<int>();
+        
+
+        do
+        {
+            Console.Write("Enter number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            if (userNumber == 0)
+            {
+                break;
+            }
+            userNumbers.Add(userNumber);
+        } while (true);
+
+        // Compute the sum, or total, of the numbers in the list.
+        int totalNum = 0;
+        foreach (int num in userNumbers)
+        {
+            totalNum += num;
+        }
+        Console.WriteLine($"The sum is: {totalNum}");
+
+        // Compute the average of the numbers in the list.
+        float userAverage = (float)totalNum / userNumbers.Count;
+        Console.WriteLine($"The average is: {userAverage}");
+
+        // Find the maximum, or largest, number in the list.
+        int largestNum = userNumbers[0];
+        foreach (int num in userNumbers)
+        {
+            if (num > largestNum)
+            {
+                largestNum = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNum}");
+
+        // Have the user enter both positive and negative numbers, then find the smallest positive number (the positive number that is closest to zero).
+        // int smallestPositive = 0;
+        // // This loop is to extract the first positive number from the list to start with
+        // foreach (int num in userNumbers)
+        // {
+        //     if (num > 0)
+        //     {
+        //         smallestPositive = num;
+        //         break;
+        //     }
+        // }
+        // // Now I run the loop again to find the smallest positive int.
+        int smallestPositive = int.MaxValue;
+        foreach (int num in userNumbers)
+        {
+            if (num < smallestPositive && num > 0)
+            {
+                smallestPositive = num;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        // Sort the numbers in the list and display the new, sorted list.
+        userNumbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in userNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
+
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        // Displays the message, "Welcome to the Program!"
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        // Asks for and returns the user's name (as a string)
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        // Asks for and returns the user's favorite number (as an integer)
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+
+        // Accepts an integer as a parameter and returns that number squared (as an integer)
+        static int SquareNumber(int rootNum)
+        {
+            return (rootNum * rootNum);
+        }
+
+        // Accepts the user's name and the squared number and displays them.
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}.");
+        }
+
+        // Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(userName, square);
+        
+
+
     }
 }
