0c98c22b573edaf1757d99685afb9400bfb8354a
trevheg <trevorhegstrom@gmail.com>
2024-01-30 16:29:24 -0800

    Week 4 Learning - Homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Trevor Coolguy", "Metals");
+        System.Console.WriteLine(assignment1.GetSummary());
+
+
+        MathAssignment mathAssignment1 = new MathAssignment("James Theman", "Calculus", "Integrals", "4-32");        
+        System.Console.WriteLine(mathAssignment1.GetSummary());
+        System.Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Neil Awesomesauce", "Fantasy", "The Wishing Well");
+        System.Console.WriteLine(writingAssignment1.GetSummary());
+        System.Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
