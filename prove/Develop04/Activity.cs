f93683e0f222efef5c253bdff2f89c3d6cd5d137
trevheg <trevorhegstrom@gmail.com>
2024-02-02 12:39:30 -0800

    prove 4 ShowSpinner()

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -21,7 +21,22 @@ public class Activity
 
     public void ShowSpinner(int seconds)
     {
-
+        var aniList = new List<string> {"|", "/", "-", "\\"};
+        int aniPlace = 0;
+        DateTime startTime = DateTime.Now;
+        DateTime stopTime = startTime.AddSeconds(seconds);
+        do
+        {
+            Console.Write(aniList[aniPlace]); 
+            Thread.Sleep(200);
+            Console.Write("\b \b");
+            aniPlace++;
+            if (aniPlace > 3)
+            {
+                aniPlace = 0;
+            }
+        } while (DateTime.Now < stopTime);
+        
     }
 
     public void ShowCountDown(int seconds)
