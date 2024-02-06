c62f099e665d91f4a740942f2dc9bfdfbc851d1e
trevheg <trevorhegstrom@gmail.com>
2024-02-06 15:52:14 -0800

    shapes activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Net.WebSockets;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 3);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("green", 4, 5);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("yellow", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+
     }
 }
