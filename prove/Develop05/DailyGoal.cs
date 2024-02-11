5a358564f5669098e67da7ba4b381ad6c811f8a7
trevheg <trevorhegstrom@gmail.com>
2024-02-10 18:45:16 -0800

    Began DailyGoal()

---
+++ prove/Develop05/DailyGoal.cs
@@ -0,0 +1,41 @@
+public class DailyGoal : Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+    private bool _isComplete;
+    private DateTime _lastCompleted;
+
+    public DailyGoal(string name, string description, int points, bool isComplete, DateTime lastCompleted) : base(name, description, points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+        _isComplete = isComplete;
+    }
+
+    public override int RecordEvent()
+    {
+        _isComplete = true;
+        return _points;
+    }
+    public bool IsComplete()
+    {
+        return _isComplete;
+    }
+    public override string GetDetailsString()
+    {
+        string checkbox = "[ ]";
+        if (IsComplete())
+        {
+            checkbox = "[X]";
+        }
+        string oldString = base.GetDetailsString();
+        return $"{checkbox} {oldString}";
+    }
+    public override string GetStringRepresentation()
+    {
+        
+        return $"{this.GetType()}|{_shortName}|{_description}|{_points}|{_isComplete}";
+    }
+}
