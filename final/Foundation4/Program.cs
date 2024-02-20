b0e397feafc4b82007660e7e47dd027c54e61b7c
trevheg <trevorhegstrom@gmail.com>
2024-02-20 11:58:00 -0800

    took out some unnecessary code

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,10 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        List<Activity> activities = new List<Activity>();
-        activities.Add(new Running("Feb 2, 2024", 27, 5000));
-        activities.Add(new StationaryBicycle("Jan 30, 2024", 36, 24));
-        activities.Add(new SwimmingLaps("Jan 31, 2024", 20, 18));
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("Feb 2, 2024", 27, 5000),
+            new StationaryBicycle("Jan 30, 2024", 36, 24),
+            new SwimmingLaps("Jan 31, 2024", 20, 18)
+        };
 
         foreach (Activity activity in activities)
         {
